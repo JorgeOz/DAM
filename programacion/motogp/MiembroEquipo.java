beb9263fc2b4c6af3da217d90de883e51447e8a8
JorgeOz <jorortvil@gmail.com>
2026-05-21 12:05:18 +0200

    Add MiembroEquipo class

--- programacion/motogp/MiembroEquipo.java
+++ programacion/motogp/MiembroEquipo.java
@@ -1,7 +1,40 @@
 package motogp;
 
-public class MiembroEquipo {
-    public static void main(String[] args) {
-        System.out.println("Prueba");
+public abstract class MiembroEquipo {
+
+    protected String nombre;
+    protected int edad;
+    protected double sueldoBase;
+
+    public MiembroEquipo(String nombre, int edad, double sueldoBase) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sueldoBase = sueldoBase;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getSueldoBase() {
+        return sueldoBase;
+    }
+
+    public void setSueldoBase(double sueldoBase) {
+        this.sueldoBase = sueldoBase;
+    }
+
+    public abstract String realizarTarea();
 }
